19d19c4de1d663c6712503b0a3355ecf290a2e76
michele <michifilip2006@gmail.com>
2025-01-20 21:26:31 +0100

    refactor: update build script in package.json, switch to adapter-vercel in svelte.config.js, and clean up vite.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,4 @@
-
-
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,27 +9,5 @@ const config = {
 		adapter: adapter()
 	}
 };
+
 export default config;
-
-
-
-
-
-
-
-
-
-
-// import adapter from '@sveltejs/adapter-vercel';
-
-// /** @type {import('@sveltejs/kit').Config} */
-// const config = {
-// 	kit: {
-// 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-// 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-// 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-// 		adapter: adapter()
-// 	}
-// };
-
-// export default config;
--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	build: {
-		// Disabilita qualsiasi ottimizzazione Vite relativa ai font
-		rollupOptions: {
-		  output: {
-			assetFileNames: 'src/lib/fonts/Ubuntu-Bold.ttf' // Evita il rinominare degli asset (inclusi i font)
-			
-		  }
-		}
-	  }
+	plugins: [sveltekit()]
 });
