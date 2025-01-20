b19ee2def8503ed9508c2ab0587f22cc10055a62
michele <michifilip2006@gmail.com>
2025-01-20 17:57:45 +0100

    refactor: update build script in package.json, enhance app.css with font layer, and configure Vite to prevent font asset renaming

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,14 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	build: {
+		// Disabilita qualsiasi ottimizzazione Vite relativa ai font
+		rollupOptions: {
+		  output: {
+			assetFileNames: 'src/lib/fonts/Ubuntu-Bold.ttf' // Evita il rinominare degli asset (inclusi i font)
+			
+		  }
+		}
+	  }
 });
